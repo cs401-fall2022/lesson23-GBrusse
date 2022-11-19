aa7567656c8aacdcea4908b3d401a4e002e56ad6
GBrusse <gary.brusse@gmail.com>
2022-11-19 14:25:32 -0700

    up to date

--- mustache.js
+++ mustache.js
@@ -109,7 +109,7 @@
      * which the closing tag for that section begins.
      *
      * Tokens for partials also contain two more elements: 1) a string value of
-     * indendation prior to that tag and 2) the index of that tag on that line -
+     * indentation prior to that tag and 2) the index of that tag on that line -
      * eg a value of 2 indicates the partial is the third tag on this line.
      */
     function parseTemplate (template, tags) {
